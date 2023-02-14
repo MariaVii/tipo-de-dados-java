65e233a5d8227346c7df4ea988c1462cad9586ae
MariaVii <mav.andraade@gmail.com>
2023-02-14 14:14:38 +0000

    salvar

--- EstruturaDeSelecao.java
+++ EstruturaDeSelecao.java
@@ -1,3 +1,14 @@
 public class EstruturaDeSelecao {
+
+    public static void main(String[] args){
+
+        int idade;
+
+        if (idade >= 18)
+        {
+            System.out.print(:"apto ao Serviço Militar Obrigatório!");
+    
+        }else{
+            System.out.print(:"Não esta apto ao Serviço Militar Obrigatório!")
     
 }
