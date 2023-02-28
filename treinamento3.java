280c0a9d10cc52464192204027c54bcb89f4b4e2
MariaVii <mav.andraade@gmail.com>
2023-02-28 15:19:50 +0000

    salvar

---
+++ treinamento3.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class treinamento3 {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        double salarioAtual, salarioNovo;
+
+        System.out.println("Informe seu Salário Atual: "); 
+        salarioAtual = input.nextFloat();
+
+        if (salarioAtual > 1000 && salarioAtual < 2000) {
+            System.out.println("Seu novo salário é:  \n");
+            salarioNovo = salarioAtual * 1.15 ;
+
+        }else if (salarioAtual > 2000 && salarioAtual < 3000) {
+            salarioNovo = salarioAtual * 1.10;
+
+        }else if (salarioAtual > 2000 && salarioAtual < 3000) {
+            salarioNovo = salarioAtual * 1.05;
+        }
+    }
+}
+        
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
