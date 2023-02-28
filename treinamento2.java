a6dab7697452b63fdeff17b0e21b87f64314960a
MariaVii <mav.andraade@gmail.com>
2023-02-28 14:52:25 +0000

    Salvar

--- treinamento2.java
+++ treinamento2.java
@@ -5,19 +5,34 @@ public class treinamento2 {
     
         Scanner input = new Scanner(System.in);
 
-        int = idade;
+        String nome;
+        int idade;
      
+        System.out.println("Informe seu nome : "); 
+        nome = input.next();
+    
         System.out.println("Informe a sua idade : "); 
         idade = input.nextInt();
 
-        if (idade >= 0 && 12);
+        if (idade >= 0 && idade <= 12)
         {
             System.out.print("Você é criança!");
-    
-        }
+
+        }else if (idade >= 13 && idade <= 17){
+                System.out.print("Você é adolescente!");
+
+        }else if (idade >= 18 && idade <= 59){
+            System.out.print("Você é adulto!");
+
+        }else if(idade >60){
+            System.out.print("Você é idoso!");
+    }
+    }
+}
+        
     
 
     
 
         
-}
+
