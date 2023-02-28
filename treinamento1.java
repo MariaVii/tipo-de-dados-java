aaded5d76e511e7ba8f47c1356270713c7c3f91c
MariaVii <mav.andraade@gmail.com>
2023-02-28 14:34:22 +0000

    salvar

--- treinamento1.java
+++ treinamento1.java
@@ -5,14 +5,19 @@ public class treinamento1 {
     public static void main(String[] args){
     
         Scanner input = new Scanner(System.in);
+        int num;
 
-        System.out.print("Insira um numero! :");
-        num = input.nextFloat();
+        System.out.println("Insira um numero! :");
+        num = input.nextInt();
       
-        if (retorno == true)
-            System.out.println("O numero é positivo!");
-        } else {
-            System.out.println("O numero é negativo!");
+        if (num > 0){
+            System.out.print ("O numero é positivo!");
+        }else if( num < 0){
+
+            System.out.print ("O numero é negativo!");
+        }else 
+        {
+            System.out.print ("O numero é 0!");
         }
     }
 }
---
+++ treinamento2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class treinamento2 {
+    public static void main(String[] args){
+    
+        Scanner input = new Scanner(System.in);
+
+        int = idade;
+     
+        System.out.println("Informe a sua idade : "); 
+        idade = input.nextInt();
+
+        if (idade >= 0 && 12);
+        {
+            System.out.print("Você é criança!");
+    
+        }
+    
+
+    
+
+        
+}
