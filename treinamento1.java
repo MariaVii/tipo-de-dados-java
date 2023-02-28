f369e865222cf0b045cb4a8ad4d6a6efa9333eaf
MariaVii <mav.andraade@gmail.com>
2023-02-28 14:04:36 +0000

    salvar

---
+++ treinamento1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class treinamento1 {
+   
+    public static void main(String[] args){
+    
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Insira um numero! :");
+        num = input.nextFloat();
+      
+        if (retorno == true)
+            System.out.println("O numero é positivo!");
+        } else {
+            System.out.println("O numero é negativo!");
+        }
+    }
+}
