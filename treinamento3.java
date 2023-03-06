fef040ef83e14f5265b5e4a4f2f9805f1e0fd252
MariaVii <mav.andraade@gmail.com>
2023-03-06 10:59:54 +0000

    salvar

--- treinamento3.java
+++ treinamento3.java
@@ -4,21 +4,27 @@ public class treinamento3 {
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
-        double salarioAtual, salarioNovo;
+        double Salarionovo, salarioAtual;
 
         System.out.println("Informe seu Salário Atual: "); 
-        salarioAtual = input.nextFloat();
+        salarioAtual = input.nextDouble();
 
-        if (salarioAtual > 1000 && salarioAtual < 2000) {
-            System.out.println("Seu novo salário é:  \n");
-            salarioNovo = salarioAtual * 1.15 ;
+        if (salarioAtual >= 1000 && salarioAtual < 2000) {
+          Salarionovo = salarioAtual * 1.15 ;
 
-        }else if (salarioAtual > 2000 && salarioAtual < 3000) {
-            salarioNovo = salarioAtual * 1.10;
+        }else if (salarioAtual >= 2000 && salarioAtual < 3000) {
+            Salarionovo = salarioAtual * 1.10;
 
-        }else if (salarioAtual > 2000 && salarioAtual < 3000) {
-            salarioNovo = salarioAtual * 1.05;
+        }else if (salarioAtual >= 3000) {
+            Salarionovo = salarioAtual * 1.05;
+
+        }else {
+            Salarionovo = salarioAtual;
         }
+
+        System.out.printf("Seu novo salário é: ");
+
+
     }
 }
         
