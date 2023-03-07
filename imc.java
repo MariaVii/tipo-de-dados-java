4b7cea9634f502ff4f822c830b133e3fddb8dcc8
MariaVii <mav.andraade@gmail.com>
2023-03-07 14:31:50 +0000

    salvar

--- imc.java
+++ imc.java
@@ -15,21 +15,20 @@ public class imc {
         imc = peso / (altura * altura);
         
         if (imc < 18.5) {
-            System.out.printf("o seu imc e" + "a faixa de peso é: abaixo do peso");
-        } else if (imc <= 24.9){    
+            System.out.printf("o seu imc é" + "a faixa de peso é: Abaixo do peso");
+        } else if (imc <= 19 && imc <= 24.9){    
 
-           System.out.printf("o seu imc e" + "a faixa de peso é: Peso normal ");
-        } else if (imc <= 29.9){    
+           System.out.printf("o seu imc é" + "a faixa de peso é: Peso normal ");
+        } else if (imc <= 25.0 && imc <=  29.9){    
      
-            System.out.printf("o seu imc e" + "a faixa de peso é: Sobrepeso");
-        } else if (imc <= 34.9){    
+            System.out.printf("o seu imc é" + "a faixa de peso é: Sobrepeso");
+        } else if (imc <= 30.0 && imc <=  34.9 ){    
          
-            System.out.printf("o seu imc e" + "a faixa de peso é: Obesidade grau 1");
-        } else if (imc <= 39.9){    
+            System.out.printf("o seu imc é" + "a faixa de peso é: Obesidade grau 1");
+        } else if (imc <= 35.0 && imc <= 39.9){    
 
-
-            System.out.printf("o seu imc e" + "a faixa de peso é: Obesidade grau 2");
-        } else if (imc <= 34.9){   
+            System.out.printf("o seu imc é" + "a faixa de peso é: Obesidade grau 2");
+        } else if (imc <= 40.0 ){   
 
 
 
